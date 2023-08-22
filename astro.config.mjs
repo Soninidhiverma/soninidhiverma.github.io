2c7d4818a64af3c11a449289b15936c499d2dcc9
soninidhiverma <p23cs0010@iitj.ac.in>
2023-08-23 02:04:26 +0530

    Init commit
    Signed-off-by: soninidhiverma <p23cs0010@iitj.ac.in>

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://sagarbaba.github.io",
+  server: {
+    port: 4000,
+  },
+});
